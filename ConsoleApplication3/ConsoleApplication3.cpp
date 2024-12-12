5dddce1ab01ffab4219ed5c882fc3793df3d5de6
nshim <nshim@DESKTOP-V5DJPRA>
2024-12-12 21:38:27 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,18 @@
+﻿﻿#include <iostream>
+#include "MyLibrary.h"
+using namespace std;
+
+int main()
+{
+    Office obj1("OfficePC", "Intel i7", "Integrated", "512GB", "16GB", "MS Office");
+    obj1.Print();
+    cout << "Price: " << obj1.GetPrice() << " USD" << endl << endl;
+
+    Game obj2("GamingPC", "AMD Ryzen 5", "NVIDIA RTX 4090", "1TB", "32GB", "Mechanical Keyboard", "Gaming Headset");
+    obj2.Print();
+    cout << "Price: " << obj2.GetPrice() << " USD" << endl << endl;
+
+    Home obj3("HomePC", "Intel i9", "Integrated", "1TB", "16GB", "Logitech Webcam");
+    obj3.Print();
+    cout << "Price: " << obj3.GetPrice() << " USD" << endl << endl;
+}
---
+++ ConsoleApplication3/myLibrary.h
@@ -0,0 +1,115 @@
+#pragma once
+#include <iostream>
+#include "MyLibrary.h"
+using namespace std;
+
+class PC
+{
+protected:
+
+    string name;
+    string cpu;
+    string gpu;
+    string ssd;
+    string ram;
+    double price;
+
+public:
+
+    PC() {}
+    PC(string nm, string cp, string gp, string ss, string rm)
+    {
+        name = nm;
+        cpu = cp;
+        gpu = gp;
+        ssd = ss;
+        ram = rm;
+        price = 0.0;
+    }
+    void Print()
+    {
+        cout << "Name: " << name << endl << "CPU: " << cpu << endl << "GPU: " << gpu << endl << "SSD: " << ssd << endl << "RAM: " << ram << endl;
+    }
+    virtual double GetPrice()
+    {
+        return price;
+    }
+};
+
+class Office :public PC
+{
+private:
+
+    string micro;
+
+public:
+
+    Office() {}
+    Office(string nm, string cp, string gp, string ss, string rm, string mic) : PC(nm, cp, gp, ss, rm)
+    {
+        micro = mic;
+    }
+    void Print()
+    {
+        PC::Print();
+        cout << "Micro: " << micro << endl;
+    }
+    double GetPrice()
+    {
+        price = 28000;
+        return price;
+    }
+};
+
+class Game :public PC
+{
+private:
+
+    string keyboard;
+    string headphones;
+
+public:
+
+    Game() {}
+    Game(string nm, string cp, string gp, string ss, string rm, string kb, string hp) : PC(nm, cp, gp, ss, rm)
+    {
+        keyboard = kb;
+        headphones = hp;
+    }
+    virtual void Print()
+    {
+        PC::Print();
+        cout << "Keyboard: " << keyboard << endl;
+        cout << "Headphones: " << headphones << endl;
+    }
+    virtual double GetPrice()
+    {
+        price = 70000;
+        return price;
+    }
+};
+
+class Home :public PC
+{
+private:
+
+    string webCamera;
+
+public:
+
+    Home() {}
+    Home(string nm, string cp, string gp, string ss, string rm, string wc) : PC(nm, cp, gp, ss, rm)
+    {
+        webCamera = wc;
+        price = 20000;
+    }
+    virtual void Print()
+    {
+        PC::Print();
+        cout << "Web-Camera: " << webCamera << endl;
+    }
+    virtual double GetPrice()
+    {
+        return price;
+    }
+};
